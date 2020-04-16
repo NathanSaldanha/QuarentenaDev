246089fda37cbffc27bfac7a44ec1c9ea1dbfe35
unknown <nmariano85@gmail.com>
2020-04-16 15:19:09 -0300

    Gerando um loop

--- Sorteio com Botões e Funções/js/scripts.js
+++ Sorteio com Botões e Funções/js/scripts.js
@@ -2,6 +2,10 @@ function Executarsorteio(){
   aleatorio= Math.round(Math.random()*10)
   console.log(aleatorio)
   numero = prompt("Seu numero da sorte")
+  while (numero > 10 || numero < 0) {
+    alert("Numero invalido")
+    numero = prompt("Digite seu numero novamente")
+  }
   if (numero == aleatorio) {
     paragrafoResultado.innerHTML= "Você ganhou o sorteio"
   } else {
