425e8d51eeeb05e4859fc633ef57297fad6ee95e
unknown <nmariano85@gmail.com>
2020-04-19 23:36:31 -0300

    Criação de certificado com biblioteca js

---
+++ Certificado/js/scripts.js
@@ -0,0 +1,27 @@
+function gerar() {
+
+  for (let contador = 0; contador < 1000000; contador++) {
+
+    htmlCertificado = emitirCertificado(inputNome.value)
+  }
+
+  listLink=
+  [
+    "https://github.com/NathanSaldanha/QuarentenaDev/tree/master/Calculo%20e%20Fun%C3%A7%C3%B5es",
+    "https://github.com/NathanSaldanha/QuarentenaDev/tree/master/Condi%C3%A7%C3%B5es",
+    "https://github.com/NathanSaldanha/QuarentenaDev/tree/master/Contadores%20e%20Loops"
+  ]
+
+  listLinkHtml = ""
+  for (let contLink = 0; contLink < listLink.length; contLink+=1) {
+
+    link = listLink[contLink]
+    listLinkHtml += `<font color=46bec9 ><li><a href=${link}>${link}</a></li></font>`
+  }
+
+  document.body.innerHTML= 
+  `${htmlCertificado}  
+  <ol>  
+    ${listLinkHtml}
+  </ol>`  
+}
